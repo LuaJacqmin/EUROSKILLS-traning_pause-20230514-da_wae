19f17af05a0fa2a458de3d67603b243d7de38a64
Lua <lua@sympass.lu>
2023-05-14 11:59:08 +0200

    change script place

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const dawaeContainer = document.querySelector('.dawae-container')
+let allKuccles = [];
 let knucclesCounter = 0;
 
 const newKnuccles = setInterval(() => {
-    if(knucclesCounter <= 250){
+    if(knucclesCounter <= 100){
         const newImg = document.createElement('img')
 
         newImg.src = 'dawae.jpeg'
-        newImg.classList.add('absolute', "mix-blend-multiply")
+        newImg.classList.add('absolute', "mix-blend-multiply", "dawae-img")
         newImg.style.top = Math.floor(Math.random() * 120 - 10) + '%'
         newImg.style.left = Math.floor(Math.random() * 120 - 10) + '%'
+        newImg.draggable = true;
     
         dawaeContainer.appendChild(newImg)
         knucclesCounter++
+
+        allKuccles = document.querySelectorAll('.dawae-img')
     } else {
         clearInterval(newKnuccles)
+
+        // allKuccles.forEach(knuccles => {
+        //    knuccles.addEve
+        // })
     }
 }, 30);
+
+
