609f06fc073c9520c7083e718cb251dc2f5b9bbc
Lua <lua@sympass.lu>
2023-05-14 12:33:19 +0200

    add animation on mouseover on dawae

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 const dawaeContainer = document.querySelector('.dawae-container')
 let allKuccles = [];
 let knucclesCounter = 0;
+const imgNumber = 250;
+const popDelay = 30;
+let hueRotateValue = 0;
 
+document.body.style.cursor = "not-allowed"
 const newKnuccles = setInterval(() => {
-    if(knucclesCounter <= 100){
+    if(knucclesCounter <= imgNumber){
         const newImg = document.createElement('img')
 
-        newImg.src = 'dawae.jpeg'
-        newImg.classList.add('absolute', "mix-blend-multiply", "dawae-img")
+        newImg.src = 'dawae.png'
+        newImg.classList.add('absolute', "dawae-img", "transition-all", "duration-200", "ease-out")
         newImg.style.top = Math.floor(Math.random() * 120 - 10) + '%'
         newImg.style.left = Math.floor(Math.random() * 120 - 10) + '%'
-        newImg.draggable = true;
+        // newImg.draggable = true;
     
         dawaeContainer.appendChild(newImg)
         knucclesCounter++
-
-        allKuccles = document.querySelectorAll('.dawae-img')
     } else {
         clearInterval(newKnuccles)
+        document.body.style.cursor = "initial"
+        
+        allKuccles = document.querySelectorAll('.dawae-img')
 
-        // allKuccles.forEach(knuccles => {
-        //    knuccles.addEve
-        // })
+        allKuccles.forEach(knuccles => {
+            knuccles.addEventListener('mouseover', e => {
+                knuccles.style.top = Math.floor(Math.random() * 120 - 10) + '%'
+                knuccles.style.left = Math.floor(Math.random() * 120 - 10) + '%'
+                knuccles.style.filter = `hue-rotate(${hueRotateValue}deg)`
+                hueRotateValue += 5;
+            })      
+        })
     }
-}, 30);
-
-
+}, popDelay);
